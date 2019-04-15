86fa2bff5ba9e663b4d5f943680fc9c08e4c6b42
Iván Sánchez <mazyvan@hotmail.com>
2019-04-15 04:04:48 -0500

    make use of the new modules in the aplication 👌

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from './config.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ConfigModule],
+  exports: [ConfigModule],
 })
 export class AppModule {}
