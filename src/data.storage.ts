98c4d0f2a9bb12a0c9d89535b22f989ffafbb986
Iván Sánchez <mazyvan@hotmail.com>
2019-04-15 11:32:15 -0500

    add note on data storage file

--- src/data.storage.ts
+++ src/data.storage.ts
@@ -4,6 +4,9 @@ const PhoneNumbersAndVerificationCodesDataStorage: { [key: string]: number } = {
  * This file contains the data used by the application. In a real world scenario. We should consider to
  * use Redis, Mongo or a relational database to persist data after a process restart and to make this
  * project stateless.
+ *
+ * Note: if we set a persist and stateless storage to this file, remember to use a different configuration
+ * for the tests.
  */
 export class DataStorage {
   static setVerificationCodeToPhoneNumber(toSerialized: string, code: number): void {
